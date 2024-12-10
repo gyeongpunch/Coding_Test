d20b02de4a7381b3cd8eae8b24ce361ffb075b68
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2024-12-10 21:18:57 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (int i=1; i<=sqrt(n); i++){
+        if (n%i==0){
+            if (i*i!=n){
+                answer += i+n/i;
+            }
+            else{
+                answer += i;
+            }
+        }
+    }
+    
+    return answer;
+}
