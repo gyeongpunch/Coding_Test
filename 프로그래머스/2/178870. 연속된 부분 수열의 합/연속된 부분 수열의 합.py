974cbacf1a88d3f68525271c530122cd3cd3da70
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2024-12-11 17:27:21 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 384.05 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,26 +1,31 @@
 def solution(sequence, k):
     answer = []
+    n = len(sequence)
+    left, right = 0, 0
+    current_sum = sequence[0]
     
-    length = len(sequence)
-    start, end = length-1, length-1
-    curr = sequence[length-1]
+    min_length = n + 1
+    best_start, best_end = 0, 0
     
-    print(length)
-    
-    mx_l = length
-    
-    while start != -1 and end != -1:
-        if curr < k:
-            start -= 1
-            curr += sequence[start]
-        elif curr > k:
-            curr -= sequence[end]
-            end -= 1
+    while left <= right and right < n:
+        if current_sum == k:
+            window_length = right - left + 1
+            
+            if window_length < min_length:
+                min_length = window_length
+                best_start, best_end = left, right
+                
+            elif window_length == min_length and left < best_start:
+                best_start, best_end = left, right
+
+            current_sum -= sequence[left]
+            left += 1
+        elif current_sum < k:
+            right += 1
+            if right < n:
+                current_sum += sequence[right]
         else:
-            if end - start <= mx_l:
-                answer = [start, end]
-                mx_l = end - start
-            start -= 1
-            curr += sequence[start]
+            current_sum -= sequence[left]
+            left += 1
     
-    return answer
+    return [best_start, best_end]
