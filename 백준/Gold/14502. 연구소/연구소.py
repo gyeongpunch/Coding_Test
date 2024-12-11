cb8d0078dfd7d7a3b68ef6e7390f066be256db15
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2024-12-11 13:37:10 +0900

    [Gold IV] Title: 연구소, Time: 1312 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,58 +1,58 @@
 from collections import deque
+def bfs(tlst):
+    # [0] 3개 좌표를 1로 저장=> 벽 막기
+    for i,j in tlst:
+        arr[i][j]=1
+
+    # [1] 변수 및 큐 생성, 초기화
+    q = deque()
+    w = [[0]*M for _ in range(N)]
+    cnt = CNT-3     # 남은 0의 개수(max값 찾을 변수)
+
+    for ti,tj in virus:
+        q.append((ti,tj))
+        w[ti][tj]=1
+
+    # [2] 큐에 데이터 있는 동안 한개꺼내서 처리
+    while q:
+        ci,cj = q.popleft()
+        # 네방향, 범위내, 미방문, 조건(==0)
+        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<N and 0<=nj<M and w[ni][nj]==0 and arr[ni][nj]==0:
+                q.append((ni,nj))
+                w[ni][nj]=1
+                cnt-=1
+
+    # [-1] 3개 좌표를 0로 저장=> 벽 해체
+    for i,j in tlst:
+        arr[i][j]=0
+    return cnt      # 남아있는 0(빈칸) 개수
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-tmp_arr = [arr[i][:] for i in range(N)]
-visited = [[0]*M for _ in range(N)]
 
-dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
-result = 0
-total = N*M
+# [1] 빈칸 위치, 바이러스 위치를 저장
+lst = []
+virus = []
+for i in range(N):
+    for j in range(M):
+        if arr[i][j]==0:
+            lst.append((i,j))
+        elif arr[i][j]==2:
+            virus.append((i,j))
 
-def bfs(x, y):
-    q = deque([(x, y)])
-    visited[x][y] = 1
+CNT = len(lst)
+v = [0]*CNT
+ans = 0
 
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx, ny = x+dx[i], y+dy[i]
-            if 0<=nx<N and 0<=ny<M and arr[nx][ny] == 0 and visited[nx][ny] == 0:
-                q.append((nx, ny))
-                arr[nx][ny] = 2
-                visited[nx][ny] = 1
+# [1] 백트래킹으로 풀이: 1000mS
+# dfs(0, [])
+# print(ans)
 
-
-def get_virus():
-    for i in range(N):
-        for j in range(M):
-            if arr[i][j] == 2 and visited[i][j] == 0:
-                bfs(i, j)
-
-def get_safe():
-    cnt = 0
-    for i in range(N):
-        for j in range(M):
-            if arr[i][j] == 0:
-                cnt += 1
-    return cnt
-
-for i in range(total-2):
-    ix, iy = i//M, i%M
-    if arr[ix][iy] == 0:
-        for j in range(i+1, total-1):
-            jx, jy = j//M, j%M
-            if arr[jx][jy] == 0:
-                for k in range(j+1, total):
-                    kx, ky = k//M, k%M
-                    if arr[kx][ky] == 0:
-                        arr[ix][iy] = arr[jx][jy] = arr[kx][ky] = 1
-                        
-                        get_virus()
-
-                        result = max(result, get_safe())
-
-                        arr = [tmp_arr[i][:] for i in range(N)]
-                        visited = [[0]*M for _ in range(N)]
-
-print(result)
+# [2] 루프 CNT개중에서 3개를 선택(가능한 모든 조합): 300mS
+for i in range(CNT-2):
+    for j in range(i+1, CNT-1):
+        for k in range(j+1, CNT):
+            ans = max(ans, bfs([lst[i],lst[j],lst[k]]))
+print(ans)
