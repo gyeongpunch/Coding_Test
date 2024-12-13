ca66cbecb92075353c866444f685f3ff0a3297da
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2024-12-13 16:41:46 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2644 ms, Memory: 72848 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+N, M = map(int, input().split())
+arr = [list(input()) for _ in range(N)]
+visited = [[0]*M for _ in range(N)]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+def bfs():
+    q = deque([(0, 0, 1, 1)])
+    visited[0][0] = 1
+
+    while q:
+        x, y, chance, t = q.popleft()
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if nx==N-1 and ny==M-1:
+                return t+1
+
+            if 0<=nx<N and 0<=ny<M:
+                if chance==1:
+                    if visited[nx][ny]!=1:
+                        if arr[nx][ny]=='0': 
+                            q.append((nx, ny, chance, t+1))
+                            visited[nx][ny] = 1
+                        else:
+                            q.append((nx, ny, 0, t+1))
+                            visited[nx][ny] = 2
+                else:
+                    if visited[nx][ny]==0 and arr[nx][ny]=='0':
+                        q.append((nx, ny, chance-1, t+1))
+                        visited[nx][ny] = 2
+
+    return visited[N-1][M-1] if visited[N-1][M-1]!=0 else -1
+
+print(bfs())
