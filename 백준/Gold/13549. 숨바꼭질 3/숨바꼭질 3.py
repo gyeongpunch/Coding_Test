2be7bbd9f4ac5de36a59537b96d4c9f14fdff929
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2024-12-13 18:04:17 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 180 ms, Memory: 40168 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+N, K = map(int, input().split())
+visited = [-1]*200001
+
+def bfs(N, K):
+    q = deque([N])
+    visited[N] = 0
+
+    while q:
+        X = q.popleft()
+        
+        next = [X * 2, X + 1, X - 1]
+
+        for np in next:
+            if 0 <= np <= 200000:
+                if np == X * 2:
+                    if visited[np]==-1 or (visited[np]!=-1 and visited[np]>visited[X]):
+                        q.append(np)
+                        visited[np] = visited[X]
+                else:
+                    if visited[np]==-1 or (visited[np]!=-1 and visited[np]>visited[X]+1):
+                        q.append(np)
+                        visited[np] = visited[X] + 1
+    # print(visited[:50])
+    return visited[K]
+
+print(bfs(N, K))
