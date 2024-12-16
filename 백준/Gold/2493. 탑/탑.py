9d265ef1a72583e420fedd61bb7e3231775a90d4
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2024-12-17 01:56:16 +0900

    [Gold V] Title: 탑, Time: 592 ms, Memory: 110696 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -8,11 +8,9 @@ stack = []
 for i in range(N-1, -1, -1):
     if not stack:
         stack.append((data[i], i))
-        continue
     else:
-        if stack[-1][0] < data[i]:
-            while stack and stack[-1][0] < data[i]:
-                num, loc = stack.pop()
-                result[loc] = i+1
+        while stack and stack[-1][0] < data[i]:
+            num, loc = stack.pop()
+            result[loc] = i+1
         stack.append((data[i], i))
 print(*result)
