88871652d2c2b09e0c889635017319a06b3b09ed
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2024-12-21 23:25:42 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 4.94 ms, Memory: 26.1 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
 
 ### 성능 요약
 
-메모리: 26.9 MB, 시간: 6.13 ms
+메모리: 26.1 MB, 시간: 4.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 21일 23:06:21
+2024년 12월 21일 23:25:39
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -2,42 +2,41 @@
 #include <vector>
 #include <iostream>
 #include <queue>
+#include <numeric>
 
 using namespace std;
 
 int solution(vector<int> queue1, vector<int> queue2) {
     int answer = 0;
-    long long left = 0, right = 0;
-    int total = queue1.size();
+    int total = queue1.size() * 4;
+    long long left = accumulate(queue1.begin(), queue1.end(), 0ll);
+    long long right = accumulate(queue2.begin(), queue2.end(), 0ll);
+    long long target = left + right;
     
-    queue<int> q1;
-    queue<int> q2;
-    for(int i=0; i<queue1.size(); i++){
-        left += queue1[i];
-        right += queue2[i];
-        q1.push(queue1[i]);
-        q2.push(queue2[i]);
+    if(target % 2 != 0){
+        return -1;
     }
+    target /= 2;
     
+    deque<int> q1(queue1.begin(), queue1.end());
+    deque<int> q2(queue2.begin(), queue2.end());
     
-    while(left != right){
-        if(left > right){
-            left -= q1.front();
-            right += q1.front();
-            q2.push(q1.front());
-            q1.pop();
-        }
-        else if(left < right){
-            left += q2.front();
-            right -= q2.front();
-            q1.push(q2.front());
-            q2.pop();
+    while (left != right && answer <= total) {
+        if (left > right) {
+            int val = q1.front();
+            q1.pop_front();
+            left -= val;
+            right += val;
+            q2.push_back(val);
+        } else {
+            int val = q2.front();
+            q2.pop_front();
+            right -= val;
+            left += val;
+            q1.push_back(val);
         }
         answer++;
-        if(answer > total * 4){
-            return -1;
-        }
     }
     
-    return answer;
+    return left==right ? answer : -1;
 }
