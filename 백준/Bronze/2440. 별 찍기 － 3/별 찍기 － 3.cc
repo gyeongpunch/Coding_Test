7f932d0b0d1d3d1f7e0e5842aa8cd59b4ad6757c
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2024-12-22 19:18:29 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -6,10 +6,7 @@ int main(void){
     int N;
     cin >> N;
     for(int i=N; i>0; i--){
-        for(int j=0; j<i; j++){
-            cout << '*';
-        }
-        cout << endl;
+        cout << string(i, '*') << endl;
     }
     
     return 0;
