ec5b4d9b044a18503958f005ee5af2a403f53a76
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-01-04 22:02:11 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -12,32 +12,20 @@ int main(void){
 
     int N;
     cin >> N;
-    vector<vector<int>> arr(N, vector<int>(N, 0));
-    vector<vector<int>> dp(N, vector<int>(N, 0));
+    int arr[N][N];
     for(int i=0; i<N; i++){
         for(int j=0; j<=i; j++){
             cin >> arr[i][j];
         }
     }
-    
-    dp[0][0] = arr[0][0];
 
-    for(int i=1; i<N; i++){
+    for(int i=N-2; i>-1; i--){
         for(int j=0; j<=i; j++){
-            if(j==0){
-                dp[i][j] = arr[i][j] + dp[i-1][j];
-            }
-            else if(j==i){
-                dp[i][j] = arr[i][j] + dp[i-1][j-1];
-            }
-            else{
-                dp[i][j] = arr[i][j] + max(dp[i-1][j], dp[i-1][j-1]);
-            }
+            arr[i][j] = arr[i][j] + max(arr[i+1][j], arr[i+1][j+1]);
         }
     }
-    int result = *max_element(dp[N-1].begin(), dp[N-1].end());
 
-    cout << result << endl;
+    cout << arr[0][0] << '\n';
 
     return 0;
 }
