d63f7be3ec684255445c0fbad624f01a5e3c550e
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-01-05 15:51:04 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    // freopen("input.txt", "r", stdin);
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    int arr[N][2];
+    int dp[16] = {0};
+    for(int i=0; i<N; i++){
+        cin >> arr[i][0] >> arr[i][1];
+    }
+
+    for(int i=N-1; i>-1; i--){
+        if(i+arr[i][0] > N){
+            dp[i] = dp[i+1];
+        }
+        else{
+            dp[i] = max(dp[i+1], dp[i+arr[i][0]] + arr[i][1]);
+        }
+    }
+
+    cout << dp[0] << endl;
+
+    return 0;
+}   
