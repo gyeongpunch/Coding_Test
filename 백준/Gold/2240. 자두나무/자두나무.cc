1ec3731a7ea649990134229c8955360e363d152f
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-01-05 21:08:39 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    // freopen("input.txt", "r", stdin);
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T, W;
+    cin >> T >> W;
+
+    int arr[T+1];
+    int dp[1001][31] = {0};
+    int mx=0;
+
+    for(int i=1; i<=T; i++){
+        cin >> arr[i];
+        for(int j=0; j<=W; j++){
+            dp[i][j] = dp[i-1][j];
+            if(j>0){
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1]);
+            }
+            if(j%2 + 1 == arr[i]){
+                dp[i][j]++;
+                mx = max(mx, dp[i][j]);
+            }
+        }
+    }
+    
+    cout << mx << '\n';
+
+    return 0;
+}   
