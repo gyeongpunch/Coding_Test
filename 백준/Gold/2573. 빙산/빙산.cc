981bc15420820b7008dda54b44657ccd430b145e
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-01-10 21:01:23 +0900

    [Gold IV] Title: 빙산, Time: 80 ms, Memory: 3052 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -2,57 +2,30 @@
 #include <queue>
 #include <vector>
 #include <algorithm>
-#include <tuple>
 
 using namespace std;
 
-vector<vector<int>> arr;
-vector<vector<int>> visited;
-vector<pair<int, int>> ice;
-vector<pair<int, int>> new_ice;
-
+int N, M;
+int arr[300][300];
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
 
-int N, M;
+vector<pair<int, int>> ice;
+vector<pair<int, int>> new_ice;
 
-void melt(){
-    vector<tuple<int, int, int>> changes;
-
-    for(auto[x, y] : ice){
-        int cnt = 0;
-        for(int i=0; i<4; i++){
-            int nx=x+dx[i];
-            int ny=y+dy[i];
-            if(0<=nx && nx<N && 0<=ny && ny<M && arr[nx][ny]==0){
-                cnt++;
-            }
-        }
-        int new_h = max(arr[x][y] - cnt, 0);
-        if(new_h>0){
-            new_ice.push_back({x, y});
-        }
-        changes.push_back({x, y, new_h});
-    }
-    for(auto[x, y, h] : changes){
-        arr[x][y] = h;
-    }
-    ice = new_ice;
-    new_ice.clear();
-}
-
-void bfs(int x, int y){
+void bfs(int x, int y, vector<vector<int>>& visited){
     queue<pair<int, int>> q;
     q.push({x, y});
     visited[x][y] = 1;
 
     while(!q.empty()){
-        auto[x, y] = q.front();
+        auto[cx, cy] = q.front();
         q.pop();
+
         for(int i=0; i<4; i++){
-            int nx=x+dx[i];
-            int ny=y+dy[i];
-            if(0<=nx && nx<N && 0<=ny && ny<M && arr[nx][ny]!=0 && !visited[nx][ny]){
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+            if(0<=nx && nx<N && 0<=ny && ny<M && arr[nx][ny]>0 && visited[nx][ny]==0){
                 q.push({nx, ny});
                 visited[nx][ny] = 1;
             }
@@ -60,60 +33,74 @@ void bfs(int x, int y){
     }
 }
 
-bool count_ice(){
-    visited = vector<vector<int>>(N, vector<int>(M, 0));
+int Count(){
+    vector<vector<int>> visited(N, vector<int>(M, 0));
+
     int cnt=0;
     for(auto[x, y] : ice){
-        if(!visited[x][y]){
-            if(cnt==1){
-                return true;
-            }
-            bfs(x, y);
+        if(arr[x][y] > 0 && visited[x][y]==0){
+            bfs(x, y, visited);
             cnt++;
+            if(cnt >= 2){
+                return cnt;
+            }
         }
     }
-    return false;
+    return cnt;
 }
 
-int simulation(){
-    int total = 0;
-
-    while(1){
-        melt();
-
-        total++;
-
-        if(count_ice()){
-            return total;
+void Melt(){
+    vector<tuple<int, int, int>> change;
+    for(auto[x, y] : ice){
+        int cnt = 0;
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(0<=nx && nx<N && 0<=ny && ny<M && arr[nx][ny]==0){
+                cnt++;
+            }
         }
-
-        if(ice.empty()){
-            return 0;
+        int rest = max(arr[x][y]-cnt, 0);
+        change.push_back({x, y, rest});
+        if(rest>0){
+            new_ice.push_back({x, y});
         }
     }
+    for(auto[x, y, i] : change){
+        arr[x][y] = i;
+    }
+    ice = new_ice;
+    new_ice.clear();
 }
 
 int main(void){
     // freopen("input.txt", "r", stdin);
 
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
     cin >> N >> M;
-    arr = vector<vector<int>>(N, vector<int>(M, 0));
-
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             cin >> arr[i][j];
-            if(arr[i][j] != 0){
+            if(arr[i][j]>0){
                 ice.push_back({i, j});
             }
         }
     }
 
-    int result = simulation();
+    int result = 0;
+    while(1){
+        Melt();
+        result++;
+        int num = Count();
+        if(num >= 2){
+            break;
+        }
+        else if(num==0){
+            result = 0;
+            break;
+        }
+    }
 
-    cout << result << endl;
+    cout << result << '\n';
 
     return 0;
 }
