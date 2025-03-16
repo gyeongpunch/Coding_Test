5e0f5df949041d82a5a6d45138cbff0cd5417538
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-03-16 10:13:41 +0900

    Title: 2048 게임, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 250316/2048 게임/2048-game.cpp
+++ 250316/2048 게임/2048-game.cpp
@@ -1,165 +1,163 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstring>
+#include <string>
 
 using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-struct Block{
-    int num;
-    bool isAvail;
+// 중복순열을 만들어서 5번 이동시키고 최대값을 찾아봅시다.
+// 반시계 회전 rotate 함수를 만듭니다.
+// 아래: 0, 왼쪽: 1, 위쪽: 2, 오른쪽: 3
+
+// d가 0이면 drop만 합니다.
+// 1이상이면 d만큼 회전 후 drop하고 4-d만큼 회전합니다.
+
+// drop함수는 rowTmp(내려올 위치)가 N-1이면 그냥 쭉 내려옵니다.
+// N-1이 아니면 합쳐질 수 있나 없나 확인합니다.
+// 합쳐질 수 있으면 rowTmp는 그대로두고 rowTmp+1에 있는 값을 2배 후, row에 있는 값을 0으로 만듭니다.
+// 합칠 수 없으면 rowTmp에 내리고 row에 있는 값을 0으로 만듭니다.
+
+struct Node {
+	int num;
+	bool isavail;
 };
 
 const int MAX_N = 20;
-const int MAX_CNT = 5;
-
+const int MAX_DEPTH = 5;
 int N;
 int arrOrigin[MAX_N][MAX_N];
-int arr[MAX_N][MAX_N];
-Block arrTmp[MAX_N][MAX_N];
-vector<vector<int>> totalPermu;
-vector<int> onePermu;
+Node arr[MAX_N][MAX_N];
+int arrTmp[MAX_N][MAX_N];
+vector<vector<int>> dirPermu;
 int result = 0;
 
-void makePermu(int depth){
-    if(depth == MAX_CNT){
-        totalPermu.push_back(onePermu);
-
-        // for(int a : onePermu){
-        //     cout << a << ' ';
-        // }
-        // cout << '\n';
-
-        return;
-    }
-
-    for(int i=0; i<4; i++){
-        onePermu.push_back(i);
-
-        makePermu(depth + 1);
-
-        onePermu.pop_back();
-    }
+void makePermutation(int depth, vector<int> &path) {
+	if (depth == MAX_DEPTH) {
+		dirPermu.push_back(path);
+		return;
+	}
+	for (int i = 0; i < 4; i++) {
+		path.push_back(i);
+		makePermutation(depth + 1, path);
+		path.pop_back();
+	}
 }
 
-void init(){
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            arr[i][j] = arrOrigin[i][j];
-        }
-    }
+void initArr() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			arr[i][j] = { arrOrigin[i][j], true };
+		}
+	}
 }
 
-void rotate(){
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            arrTmp[N-j-1][i].num = arr[i][j];
-            arrTmp[i][j].isAvail = true;
-        }
-    }
+// 반시계 방향 회전
+void rotate() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			arrTmp[i][j] = arr[j][N - i - 1].num;
+		}
+	}
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            arr[i][j] = arrTmp[i][j].num;
-        }
-    }
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			arr[i][j].num = arrTmp[i][j];
+		}
+	}
 }
 
-void drop(){
-    for(int j=0; j<N; j++){
-        int nowI = N-1;
-        for(int i=N-1; i>-1; i--){
-            if(arrTmp[i][j].num == 0) continue;
-            
-            if(nowI == N-1){
-                if(i == nowI){
-                    nowI--;
-                }
-                else{
-                    arrTmp[nowI][j].num = arrTmp[i][j].num;
-                    arrTmp[i][j].num = 0;
-                    nowI--;
-                }
-            }
-            else{
-                if(!arrTmp[nowI + 1][j].isAvail || arrTmp[nowI + 1][j].num != arrTmp[i][j].num){
-                    if(i == nowI){
-                        nowI--;
-                    }
-                    else{
-                        arrTmp[nowI][j].num = arrTmp[i][j].num;
-                        arrTmp[i][j].num = 0;
-                        nowI--;
-                    }
-                }
-                else{
-                    arrTmp[nowI + 1][j].num *= 2;
-                    arrTmp[nowI + 1][j].isAvail = false;
-                    arrTmp[i][j].num = 0;
-                }
-            }
-        }
-    }
+void drop() {
+	for (int col = 0; col < N; col++) {
+		int rowTmp = N - 1;
+		for (int row = N - 1; row > -1; row--) {
+			if (arr[row][col].num > 0) {
+				if (rowTmp == N - 1) {
+					if (rowTmp == row) {
+						rowTmp--;
+					}
+					else {
+						arr[rowTmp--][col].num = arr[row][col].num;
+						arr[row][col].num = 0; 
+					}
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            arr[i][j] = arrTmp[i][j].num;
-            arrTmp[i][j].isAvail = true;
-        }
-    }
+				}
+				else {
+					if (arr[row][col].num == arr[rowTmp + 1][col].num && arr[rowTmp + 1][col].isavail) {
+						arr[rowTmp + 1][col].num *= 2;
+						arr[rowTmp + 1][col].isavail = false;
+						arr[row][col].num = 0;
+					}
+					else {
+						if (rowTmp == row) {
+							rowTmp--;
+						}
+						else {
+							arr[rowTmp--][col].num = arr[row][col].num;
+							arr[row][col].num = 0;
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
-int calc(){
-    int mx = 0;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            mx = max(mx, arr[i][j]);
-        }
-    }
-    return mx;
+void simulation() {
+	for (const vector<int> &dirs : dirPermu) {
+		initArr();
+
+		for (const int &d : dirs) {
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					arr[i][j].isavail = true;
+				}
+			}
+
+			if (d == 0) {
+				drop();
+				continue;
+			}
+
+			for (int i = 0; i < d; i++) {
+				rotate();
+			}
+
+			drop();
+
+			for (int i = 0; i < 4 - d; i++) {
+				rotate();
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				result = max(result, arr[i][j].num);
+			}
+		}
+	}
 }
 
-void simulation(const vector<int> &cmds){
-    init();
 
-    for(const int &cmd : cmds){
-        if(cmd == 0){
-            for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++){
-                    arrTmp[i][j].num = arr[i][j];
-                    arrTmp[i][j].isAvail = true;
-                }
-            }
 
-            drop();
-            continue;
-        }
+int main(void) {
+	fastio;
 
-        for(int i=0; i<cmd; i++){
-            rotate();
-        }
-        drop();
-        for(int i=0; i<4-cmd; i++){
-            rotate();
-        }
-    }
-    result = max(result, calc());
-}
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> arrOrigin[i][j];
+		}
+	}
 
-int main() {
-    cin >> N;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            cin >> arrOrigin[i][j];
-        }
-    }
-    
-    makePermu(0);
+	vector<int> path;
+	makePermutation(0, path);
 
-    for(const vector<int> &cmds : totalPermu){
-        simulation(cmds);
-    }
+	simulation();
 
-    cout << result << '\n';
+	cout << result << '\n';
 
-    return 0;
+	return 0;
 }
