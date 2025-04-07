3fa3bb8fc38e9208925e71532ea60121046a3d92
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-04-07 22:35:38 +0900

    Title: 왕실의 기사 대결, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250407/왕실의 기사 대결/royal-knight-duel.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define MAX_L 40
+#define MAX_N 30
+
+struct Night{
+    int x, y, h, w, k;
+};
+
+struct Point{
+    int x, y;
+};
+void nprint();
+
+int L, N, Q;
+int arr[MAX_L][MAX_L];
+int arrNight[MAX_L][MAX_L];
+int arrNightTmp[MAX_L][MAX_L];
+int visited[MAX_L][MAX_L];
+Night night[MAX_N+1];
+int damage[MAX_N+1];
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+bool bound_check(int x, int y){
+    return 0<=x && x<L && 0<=y && y<L;
+}
+
+void push_night(int idx, int dir, int is_hurt){
+    Night &now = night[idx];
+
+    int nx, ny;
+    for(int i=now.x; i<now.x+now.h; i++){
+        for(int j=now.y; j<now.y+now.w; j++){
+            nx = i + dx[dir];
+            ny = j + dy[dir];
+
+            if(!bound_check(nx, ny)) return;
+            if(arr[nx][ny] == 2) return;
+
+            if(arrNight[nx][ny] != 0 && idx != arrNight[nx][ny]){
+                push_night(arrNight[nx][ny], dir, 1);
+                
+
+                if(arrNight[nx][ny] != 0 && arrNight[i][j] != arrNight[nx][ny]){
+                    return;
+                }
+            }
+        }
+    }
+    // nprint();
+    // cout << "idx : " << idx << ' ' << is_hurt << '\n';
+    for(int i=now.x; i<now.x+now.h; i++){
+        for(int j=now.y; j<now.y+now.w; j++){
+            arrNight[i][j] = 0;
+        }
+    }
+
+    now.x += dx[dir];
+    now.y += dy[dir];
+
+    for(int i=now.x; i<now.x+now.h; i++){
+        for(int j=now.y; j<now.y+now.w; j++){
+            arrNight[i][j] = idx;
+
+            if(arr[i][j] == 1 && is_hurt == 1){
+                damage[idx]++;
+            }
+        }
+    }
+
+    if(damage[idx] >= now.k){
+        for(int i=now.x; i<now.x+now.h; i++){
+            for(int j=now.y; j<now.y+now.w; j++){
+                arrNight[i][j] = 0;
+            }
+        }
+    }
+
+}
+
+void simulation(int idx, int dir){
+    
+    push_night(idx, dir, 0);
+}
+
+int get_result(){
+    int sum = 0;
+    for(int i=1; i<=N; i++){
+        if(night[i].k > damage[i]){
+            sum += damage[i];
+        }
+    }
+    return sum;
+}
+
+int main(void){
+    fastio;
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> L >> N >> Q;
+    for(int i=0; i<L; i++){
+        for(int j=0; j<L; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    int x, y, h, w, k;
+    for(int i=1; i<=N; i++){
+        cin >> x >> y >> h >> w >> k;
+        x--;
+        y--;
+        night[i] = {x, y, h, w, k};
+        for(int j=x; j<x+h; j++){
+            for(int k=y; k<y+w; k++){
+                arrNight[j][k] = i;
+            }
+        }
+    }
+    // nprint();
+    int idx, d;
+    for(int i=0; i<Q; i++){
+        cin >> idx >> d;
+
+        simulation(idx, d);
+        // nprint();
+    }
+
+    cout << get_result() << '\n';
+
+    return 0;
+}
+
+void nprint(){
+    cout << "===============\n";
+    for(int i=0; i<L; i++){
+        for(int j=0; j<L; j++){
+            cout << arrNight[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    cout << "===============\n";
+}
