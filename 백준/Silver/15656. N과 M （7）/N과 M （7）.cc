53dc326c320fa3e37ea4128945da0f8d0bd68492
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-06-06 12:19:47 +0900

    [Silver III] Title: N과 M (7), Time: 392 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(nullptr);
+
+int N, M;
+vector<int> numbers;
+vector<int> path;
+
+void get_perm(int depth){
+    if(depth==M){
+        for(int n : path){
+            cout << n << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=0; i<N; i++){
+        path.push_back(numbers[i]);
+
+        get_perm(depth+1);
+
+        path.pop_back();
+    }
+}
+
+int main(void){
+    fastio;
+    
+    // freopen("input.txt", "r", stdin);
+
+    cin >> N >> M;
+    int n;
+    for(int i=0; i<N; i++){
+        cin >> n;
+        numbers.push_back(n);
+    }
+
+    sort(numbers.begin(), numbers.end());
+
+    get_perm(0);
+
+    return 0;
+}
