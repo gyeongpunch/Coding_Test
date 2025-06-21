9e542c373353d4ced38a1a7da67869974ddf8b26
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-06-21 14:47:57 +0900

    [Silver I] Title: 부분수열의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cstring>
+
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(nullptr);
+
+const int MX=20;
+
+int N;
+int arr[MX];
+
+int main(void){
+    fastio;
+    
+    // freopen("input.txt", "r", stdin);
+
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr+N);
+
+    int target = 1;
+    for(int i=0; i<N; i++){
+        if(arr[i] > target) break;
+        target += arr[i];
+    }
+
+    cout << target << '\n';
+
+    return 0;
+}
