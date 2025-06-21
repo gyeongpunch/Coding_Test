d2cf79f7253dd1e462105f1ef7c9bd05dae6dcee
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-06-21 21:44:53 +0900

    [Gold IV] Title: N-Queen, Time: 1872 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,35 +1,15 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <set>
-#include <map>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <cstring>
 
 using namespace std;
 #define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(nullptr);
-
-struct Point{
-    int x, y;
-};
 
 const int MX = 15;
 
 int N, total;
 int vstY[MX], vstD1[MX*2], vstD2[MX*2];
 int result;
-
-int dx[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
-int dy[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
-
-bool bound_check(int x, int y){
-    return 0<=x && x<N && 0<=y && y<N;
-}
 
 void dfs(int x){
     if(x==N) {
@@ -52,7 +32,6 @@ int main(void){
     // freopen("input.txt", "r", stdin);
 
     cin >> N;
-    total = N*N;
 
     dfs(0);
 
