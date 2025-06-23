c0851020a27ed045d2d0d6cf51c36b8dee9e8709
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-06-23 22:22:51 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 19 ms, Memory: 6,024 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.cpp
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.cpp
@@ -1,133 +1,156 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <queue>
 #include <cstring>
+#include <stack>
 
 using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(nullptr);
 
-constexpr int MAX_NODE = 5000;
+constexpr int MAX_NODE = 10000;
 constexpr int MAX_CMD = 500;
 
-struct Node {
+struct Node{
     int data;
     Node* next;
 };
 
-Node node_pool[MAX_NODE];
-int node_count;
-Node head;
+Node nodePool[MAX_NODE];
+int nodeCnt = 0;
+Node head; 
 
-Node* new_node(int data) {
-    node_pool[node_count].data = data;
-    node_pool[node_count].next = nullptr;
-    return &node_pool[node_count++];
+Node* create_node(int data){
+    if(nodeCnt >= MAX_NODE) return nullptr;
+    Node* newNode = &nodePool[nodeCnt++];
+    newNode->data = data;
+    newNode->next = nullptr;
+    return newNode;
 }
 
-void init() {
+int N, M;
+int result;
+
+void init(){
     head.next = nullptr;
-    node_count = 0;
+    nodeCnt = 0;
 }
 
-Node* getNodeAt(int x) {
+Node* get_node_at(int x){
     Node* cur = &head;
-    for (int i = 0; i < x && cur->next != nullptr; i++) {
+    for(int i=0; i<x && cur->next != nullptr; i++){
         cur = cur->next;
     }
     return cur;
 }
 
-void insert(int x, int y) {
-    Node* pos = getNodeAt(x);
-    while (y--) {
-        int val;
-        cin >> val;
-        Node* newNode = new_node(val);
-
-        newNode->next = pos->next;
-        pos->next = newNode;
-        pos = newNode;
+void insert(int x, int y){
+    Node* getNode = get_node_at(x);
+    for(int j=0; j<y; j++){
+        int data;
+        cin >> data;
+        Node* newNode = create_node(data);
+        if(newNode == nullptr) return;
+        newNode->next = getNode->next;
+        getNode->next = newNode;
+        getNode = newNode;
     }
 }
 
-void remove(int x, int y) {
-    Node* prev = getNodeAt(x);
-    Node* cur = prev->next;
+// void deleteNode(int x, int y){
+//     Node* prevNode = get_node_at(x);
+//     Node* curNode = prevNode->next;
 
-    while (y-- && cur != nullptr) {
-        cur = cur->next;
+//     while(y-- && curNode != nullptr){
+//         curNode = curNode->next;
+//     }
+//     prevNode->next = curNode;
+// }
+
+void deleteNode(int x, int y){
+    Node* prevNode = get_node_at(x);
+    Node* curNode = prevNode->next;
+
+    while(y-- && curNode != nullptr){
+        prevNode->next = curNode->next;
+        curNode = prevNode->next;
     }
-    
-    prev->next = cur;
 }
 
-void append(int y) {
+void append(int y){
+    Node* lastNode = &head;
+    while(lastNode->next != nullptr){
+        lastNode = lastNode->next;
+    }
+    for(int j=0; j<y; j++){
+        int data;
+        cin >> data;
+        Node* newNode = create_node(data);
+        if(newNode == nullptr) return;
+        lastNode->next = newNode;
+        lastNode = newNode;
+    }
+}
+
+void input(){
     Node* cur = &head;
-    while (cur->next != nullptr) {
-        cur = cur->next;
-    }
 
-    while (y--) {
-        int val;
-        cin >> val;
-        Node* newNode = new_node(val);
+    cin >> N;
+    for(int i=0; i<N; i++){
+        int data;
+        cin >> data;
+        Node* newNode = create_node(data);
         cur->next = newNode;
         cur = newNode;
     }
+
+    cin >> M;
+    for(int i=0; i<M; i++){
+        string command;
+        cin >> command;
+        if(command == "I"){
+            int x, y;
+            cin >> x >> y;
+
+            insert(x, y);
+        }
+        else if(command == "D"){
+            int x, y;
+            cin >> x >> y;
+
+            deleteNode(x, y);
+        }
+        else if(command == "A"){
+            int y;
+            cin >> y;
+            
+            append(y);
+        }
+    }
 }
 
-void printList(int t) {
-    cout << "#" << t << " ";
+void print_result(int t){
     Node* cur = head.next;
-    for (int i = 0; i < 10 && cur != nullptr; i++) {
-        cout << cur->data << " ";
+    cout << '#' << t << ' ';
+    for(int i=0; i<10 && cur != nullptr; i++){
+        cout << cur->data << ' ';
         cur = cur->next;
     }
     cout << '\n';
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+int main(void){
+    fastio;
+    
     // freopen("input.txt", "r", stdin);
 
-    for (int test_case = 1; test_case <= 10; test_case++) {
+    int T=10;
+    for(int t=1; t<=T; t++){    
         init();
-
-        int N, M;
-        cin >> N;
-
-        Node* cur = &head;
-        for (int i = 0; i < N; i++) {
-            int val;
-            cin >> val;
-            Node* newNode = new_node(val);
-            cur->next = newNode;
-            cur = newNode;
-        }
-
-        cin >> M;
-
-        while (M--) {
-            char cmd;
-            cin >> cmd;
-
-            if (cmd == 'I') {
-                int x, y;
-                cin >> x >> y;
-                insert(x, y);
-            }
-            else if (cmd == 'D') {
-                int x, y;
-                cin >> x >> y;
-                remove(x, y);
-            }
-            else if (cmd == 'A') {
-                int y;
-                cin >> y;
-                append(y);
-            }
-        }
-
-        printList(test_case);
+        input();
+        print_result(t);
     }
 
     return 0;
