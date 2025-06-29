7b8054c3d26cfc57db65a6ae85a8925453d33682
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-06-29 13:09:16 +0900

    [Platinum V] Title: 버블 소트, Time: 108 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cstring>
+
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(nullptr);
+typedef long long ll;
+
+const int MX = 500000;
+
+int N;
+int arr[MX];
+int tmp[MX];
+ll result;
+
+void merge_sort(int left, int right) {
+    if (left >= right) return;
+    int mid = (left + right) / 2;
+    merge_sort(left, mid);
+    merge_sort(mid + 1, right);
+
+    int i = left, j = mid + 1, k = left;
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            tmp[k++] = arr[i++];
+        } else {
+            tmp[k++] = arr[j++];
+            result += mid - i + 1;
+        }
+    }
+    while (i <= mid) tmp[k++] = arr[i++];
+    while (j <= right) tmp[k++] = arr[j++];
+    for (int i = left; i <= right; i++) arr[i] = tmp[i];
+}
+
+int main(void){
+    fastio;
+    
+    // freopen("input.txt", "r", stdin);
+
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+
+    merge_sort(0, N - 1);
+
+    cout << result << '\n';
+
+    return 0;
+}
