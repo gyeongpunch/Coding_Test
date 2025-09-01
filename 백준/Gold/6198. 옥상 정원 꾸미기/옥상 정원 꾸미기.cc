e6e84e004ebed1072739aa4e60df2dbd9109ef78
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-09-01 23:43:39 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 44 ms, Memory: 2992 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <numeric>
+using namespace std;
+typedef long long ll;
+
+const int MX = 80000;
+
+struct Loc{
+	int n, idx;
+};
+
+int N;
+stack<Loc> stk;
+int mark[MX];
+
+int main() {
+	cin >> N;
+	int n;
+	for(int i=0; i<N; i++){
+		cin >> n;
+		while(!stk.empty() && stk.top().n <= n){
+			Loc now = stk.top(); stk.pop();
+			mark[now.idx] = i - now.idx - 1;
+		}
+		stk.push({n, i});
+	}
+
+	while(!stk.empty()){
+		Loc now = stk.top(); stk.pop();
+		mark[now.idx] = N - now.idx - 1;
+	}
+
+	ll result = 0;
+	for(int i=0; i<N; i++){
+		result += mark[i];
+	}
+	cout << result << '\n';
+	return 0;
+}
