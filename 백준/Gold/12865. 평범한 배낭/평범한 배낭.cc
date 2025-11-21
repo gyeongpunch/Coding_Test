fbb9f523eae566215a1d7b8737f136e546debc70
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-11-21 17:53:51 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,52 +1,45 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <set>
-#include <map>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <cstring>
-
 using namespace std;
-#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(nullptr);
 
 struct Info{
-    int w, v;
-
-    Info() : w(0), v(0) {}
-    Info(int w, int v) : w(w), v(v) {}
+	int w, v;
 };
 
-const int MX = 100;
-const int MXK = 100001;
+const int MX = 101;
+const int MXP = 100001;
 
 int N, K;
 Info arr[MX];
-int dp[MXK];
+int dp[MXP];
 
-int main(void){
-    fastio;
-    
-    // freopen("input.txt", "r", stdin);
+void input(){
+	cin >> N >> K;
+	for(int i=0; i<N; i++){
+		int w, v;
+		cin >> arr[i].w >> arr[i].v;
+	}
+}
 
-    cin >> N >> K;
-    int w, v;
-    for(int i=0; i<N; i++){
-        cin >> w >> v;
-        arr[i] = Info(w, v);
-    }
-    
-    for(int i=0; i<N; i++){
-        for(int j=K; j>=arr[i].w; j--){
-            dp[j] = max(dp[j], dp[j - arr[i].w] + arr[i].v);
-        }
-    }
+void solution(){
+	for(int i=0; i<N; i++){
+		Info now = arr[i];
+		for(int j=K; j>=now.w; j--){
+			dp[j] = max(dp[j], dp[j-now.w] + now.v);
+		}
+	}
+}
 
-    cout << dp[K] << '\n';
+void get_result(){
+	cout << dp[K] << '\n';
+}
 
-    return 0;
+int main() {
+	input();
+
+	solution();
+
+	get_result();
+
+	return 0;
 }
