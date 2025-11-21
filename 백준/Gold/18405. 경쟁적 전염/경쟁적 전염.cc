d6b94c01ce5787b568413537da84e466892d061c
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-11-21 11:18:16 +0900

    [Gold V] Title: 경쟁적 전염, Time: 12 ms, Memory: 2444 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
-struct Point{
+struct Virus{
+	int type;
 	int x, y;
+	int time;
 };
 
 const int MX = 200;
@@ -14,7 +17,7 @@ int arr[MX][MX];
 int S, X, Y;
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
-vector<Point> v;
+vector<Virus> v;
 
 bool bound_check(int x, int y){
 	return 0<=x && x<N && 0<=y && y<N;
@@ -25,7 +28,7 @@ void input(){
 	for(int i=0; i<N; i++){
 		for(int j=0; j<N; j++){
 			cin >> arr[i][j];
-			if(arr[i][j] != 0) v.push_back({i, j});
+			if(arr[i][j] != 0) v.push_back({arr[i][j], i, j, 0});
 		}
 	}
 	cin >> S >> X >> Y;
@@ -33,31 +36,31 @@ void input(){
 	Y--;
 }
 
-bool CMP(const Point &a, const Point &b){
-	return arr[a.x][a.y] < arr[b.x][b.y];
+bool CMP(const Virus &a, const Virus &b){
+	return a.type < b.type;
 }
 
 void solution(){
-	int now_time = 0;
-	vector<Point> tmp;
-	for(int i=0; i<S; i++){
-		sort(v.begin(), v.end(), CMP);
-		tmp.clear();
+	sort(v.begin(), v.end(), CMP);
+		
+	queue<Virus> q;
+	for(const Virus &vi : v) q.push(vi);
 
-		for(const Point &p : v){
-			for(int i=0; i<4; i++){
-				int nx = p.x + dx[i];
-				int ny = p.y + dy[i];
+	while(!q.empty()){
+		Virus now = q.front(); q.pop();
 
-				if(!bound_check(nx, ny)) continue;
-				if(arr[nx][ny] != 0) continue;
+		if(now.time == S) break;
 
-				arr[nx][ny] = arr[p.x][p.y];
-				tmp.push_back({nx, ny});
-			}
+		for(int i=0; i<4; i++){
+			int nx = now.x + dx[i];
+			int ny = now.y + dy[i];
+
+			if(!bound_check(nx, ny)) continue;
+			if(arr[nx][ny] != 0) continue;
+
+			arr[nx][ny] = arr[now.x][now.y];
+			q.push({now.type, nx, ny, now.time+1});
 		}
-
-		v = tmp;
 	}
 }
 
